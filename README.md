bb42cf7570ac05b225c3328e94692b78680e90e6
José Andrés Lucatero <joseandreslucatero@gmail.com>
2022-03-25 19:58:31 -0600

    [FIX] Bug fixes and more prompt messages

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Agenda
-Agenda electrónica hecha en node.js
+
+Agenda electrónica hecha en node.js.
+
 Utiliza readline y file system para el manejo del input y del archivo.
+
+To Do:
+
+* [ ] Revisar que el formato de entrada de los eventos sea el correcto
+* [ ] Especificar un formato para la fecha y hora
+  * [ ] Que el programa tenga conciencia de la fecha y hora
+* [ ] Cambiar **_show events_**: que muestre una _tabla_ como Agenda de Google (con fecha en grande antes del evento)
+* [ ] Que **_add event_** lo agregue manteniendo el orden de más próximo primero
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const readline = require('readline');
-const fs = require('fs');
+import { createInterface } from 'readline';
+import { appendFile, readFile, writeFile } from 'fs';
 
-let input = readline.createInterface({
+let input = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-prompt = "Agenda> ";
+let prompt, info, eventName, data_array, save, elementErased;
+prompt = "Agenda> "
 input.setPrompt(prompt, prompt.length)
 
 input.on('line', (line) => {
@@ -20,32 +21,38 @@ input.on('line', (line) => {
     console.log("Hola! Soy tu nueva agenda electrónica, puedo realizar varias funciones dependiendo de lo que escribas en la linea de comandos, por ejemplo:\n-add event {nombre, fecha, hora}  //(crea un nuevo evento con los datos que tu proporciones)\n-erase event {nombre}  //(elimina el evento que menciones)\n-show events  //(muestra todos los eventos guardados)\n-exit  //(cierra la aplicación)");
     input.prompt();
   }
-  else if (line.substring(0, 9) == 'add event') {
+  else if (line.substring(0, 10) == 'add event ') {
     info = line.substring(10);
-    fs.appendFile('save.txt', "\n" + info, (err) => {
+    appendFile('save.txt', "\n" + info, (err) => {
       if (err) throw err;
       console.log("Se ha agregado el evento");
       input.prompt();
     });
   }
-  else if (line.substring(0, 11) == 'erase event') {
-    name = line.substring(12);
-    fs.readFile('save.txt', 'utf-8', (err, data) => {
+  else if (line.substring(0, 12) == 'erase event ') {
+    eventName = line.substring(12);
+    readFile('save.txt', 'utf-8', (err, data) => {
       if (err) throw err;
       data_array = data.split('\n');
       for (var i = 0; i < data_array.length; i++)
-        if (data_array[i].match(name))
+        if (data_array[i].split(',')[0] == eventName){
           delete data_array[i];
-      save = data_array.join('\n').trim();
-      fs.writeFile('save.txt', save, (err) => {
+          elementErased = true;
+          break;
+        }
+      save = data_array.filter(function(val) { return val !== null; }).join('\n').trim();
+      writeFile('save.txt', save, (err) => {
         if (err) throw err;
-        console.log('El evento se ha borrado');
+        if (elementErased)
+          console.log('El evento',eventName,'se ha borrado');
+        else
+          console.log('No se encontró ningún evento llamado',eventName)
         input.prompt();
       });
     });
   }
   else if (line == 'show events') {
-    fs.readFile('save.txt', 'utf-8', (err, data) => {
+    readFile('save.txt', 'utf-8', (err, data) => {
       if(!data||err)
         console.log("No tienes eventos");
       else
